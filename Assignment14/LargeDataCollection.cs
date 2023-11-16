a0ee5d1a7828aeca8577702aa8d880be658444e2
Vijayaraghavan R <rvrblr@gmail.com>
2023-11-16 15:24:11 +0530

    Assignment

---
+++ Assignment14/LargeDataCollection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private ArrayList pvtArray = new ArrayList();
+        bool disposedvalue;
+
+        public LargeDataCollection(ArrayList inputArray)
+        {
+            this.pvtArray = inputArray;
+        }
+
+        public void addData(object x)
+        {
+            this.pvtArray.Add(x);
+        }
+
+        public void removeData(object x)
+        {
+            this.pvtArray.Remove(x);
+        }
+
+        public void displayData()
+        {
+            Console.WriteLine("\nThe elements in the list are: ");
+            foreach (object x in this.pvtArray)
+            {
+                Console.WriteLine(x + "\n");
+            }
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedvalue)
+            {
+                pvtArray = null;
+                disposedvalue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Assignment14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14
+{
+    internal class Program
+    {   
+        
+        static void Main(string[] args)
+        {
+            ArrayList listx = new ArrayList();
+            Console.WriteLine("Enter the number of elements to be added to the list");
+            int len = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the elements one by one");
+            for (int i=0; i<len; i++)
+            {
+                listx.Add(Console.ReadLine());
+            }
+            LargeDataCollection ldc = new LargeDataCollection(listx);
+            Console.WriteLine("Enter the element to be added");
+            ldc.addData(Console.ReadLine() );
+            Console.WriteLine("Enter the element to be removed");
+            ldc.removeData(Console.ReadLine());
+            Console.WriteLine("All the elements of the array list are displayed: ");
+            ldc.displayData();
+            ldc.Dispose();
+            Console.ReadKey();  
+        }
+    }
+}
